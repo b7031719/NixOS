d8df54ab9d2cde96be7e075fbd3a20ff00d42d49
b7 <danfenton@pm.me>
2025-05-09 11:20:06 +0100

    added fonts, kitty and hyprland settings, rofi and autostart

--- users/b7/home.nix
+++ users/b7/home.nix
@@ -8,9 +8,20 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
+    loginExtra = ''
+      [[ "$(tty)" == "/dev/tty1" ]] && exec Hyprland
+    '';
   };
   
-  programs.kitty.enable = true;
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "JetBrains Mono Nerd Font";
+      font_size = 12;
+    };
+  };
+
+  fonts.fontconfig.enable = true;
   
   wayland.windowManager.hyprland = {
     enable = true;
@@ -18,23 +29,39 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
     systemd.enable = true;
-    settings = { "$mod" = "SUPER"; };
+    settings = {
+      "$mod" = "SUPER";
+      input = {
+        kb_layout = "gb";
+      };
+      decoration = {
+        rounding = 10;
+      };
+      misc = {
+        disable_splash_rendering = true;
+      };
+    };
     extraConfig = ''
-      monitor=,preferred,auto,auto
+      monitor=,preferred,auto,1
 
       $terminal = kitty
+      $menu = rofi -show drun
 
       exec-once = waybar
       exec-once = dbus-update-activation-environment --systemd DISPLAY XAUTHORITY WAYLAND_DISPLAY XDG_SESSION_DESKTOP=Hyprland XDG_CURRENT_DESKTOP=Hyprland XDG_SESSION_TYPE=wayland
-
-      input = {
-        kb_layout = uk
-      }
 
       $mainMod = SUPER
 
       bind = $mainMod, Q, exec, $terminal
       bind = $mainMod, M, exit,
+      bind = $mainMod, R, exec, $menu
+      bind = $mainMod, C, killactive,
+
+      # Move focus binds
+      bind = $mainMod, left, movefocus, l
+      bind = $mainMod, right, movefocus, r
+      bind = $mainMod, up, movefocus, u
+      bind = $mainMod, down, movefocus, d
       '';
   };
 
@@ -47,12 +74,16 @@
   home.packages = with pkgs; [
     polkit
     waybar
+    rofi-wayland
     brave
     tmux
     protonmail-desktop
     proton-pass
     protonvpn-gui
     conda
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.hack
   ];
   
   home.sessionVariables.NIXOS_OZONE_WL = "1";
