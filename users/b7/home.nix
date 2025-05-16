d61421f7cd4a8751c5ad71bd9cf3be108aa37be6
b7 <danfenton@pm.me>
2025-05-16 11:27:20 +0100

    fixed boot issues

--- users/b7/home.nix
+++ users/b7/home.nix
@@ -66,15 +66,6 @@
     protonmail-desktop
     conda
     xdg-utils
-    gtk3
-    gtk4
-    libsForQt5.qt5.qtbase
-    libsForQt5.qt5.qtwayland
-    noto-fonts
-    adwaita-icon-theme
-    adwaita-fonts
-    hicolor-icon-theme
-    gnome-themes-extra
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
     nerd-fonts.hack
