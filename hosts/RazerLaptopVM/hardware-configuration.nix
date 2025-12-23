e8ccc7916a65580d7fa7887ce2aea49c33f6d6dc
Dan <danfenton@pm.me>
2025-12-23 21:34:11 +0000

    Updated fmask and dmask for boot

--- hosts/RazerLaptopVM/hardware-configuration.nix
+++ hosts/RazerLaptopVM/hardware-configuration.nix
@@ -21,7 +21,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/C44F-03D3";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices = [
