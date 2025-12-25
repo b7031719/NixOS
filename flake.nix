d5c87d57a5966526f07ecc1ff741c945bdcbc9f6
b7031719 <67792104+b7031719@users.noreply.github.com>
2025-12-25 16:49:00 +0000

    Added lanzaboote

--- flake.nix
+++ flake.nix
@@ -3,12 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";   # pin home-manager package to nixpkgs for consistent versions
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";   # pin hyprland package to nixpkgs for consistent versions
-    hyprlock.url = "github:hyprwm/hyprlock";
-    hyprlock.inputs.nixpkgs.follows = "nixpkgs";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v1.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprlock = {
+      url = "github:hyprwm/hyprlock";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, hyprland, hyprlock, ...}@inputs: 
@@ -22,23 +32,24 @@
 
       # NIXOS SYSTEM CONFIGURATION
       nixosConfigurations.RazerLaptopVM = nixpkgs.lib.nixosSystem {
-	inherit system pkgs;
+        inherit system pkgs;
         modules = [
-	  ./hosts/RazerLaptopVM/configuration.nix
-	  ./hosts/RazerLaptopVM/hardware-configuration.nix
-	];
+          ./hosts/RazerLaptopVM/configuration.nix
+          ./hosts/RazerLaptopVM/hardware-configuration.nix
+          inputs.lanzaboote.nixosModules.lanzaboote
+        ];
       };
       
 
       # HOME-MANAGER CONFIGURATION (STANDALONE)
       homeConfigurations = { 
         b7 = home-manager.lib.homeManagerConfiguration {
-	  inherit pkgs;
-  	  modules = [ ./users/b7/home.nix ];
-  	  extraSpecialArgs = {
-  	    inherit hyprland hyprlock;
-  	  };
-	};
+          inherit pkgs;
+          modules = [ ./users/b7/home.nix ];
+          extraSpecialArgs = {
+            inherit hyprland hyprlock;
+  	      };
+	      };
       };
 
     };
