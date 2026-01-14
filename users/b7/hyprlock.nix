bafbe2d2d4c04f0e36b8b5c57f8cb7ebae990258
b7 <danfenton@pm.me>
2026-01-14 07:05:18 +0000

    Changed hyprlock config to symlink from dotfiles repo

--- users/b7/hyprlock.nix
+++ users/b7/hyprlock.nix
@@ -1,63 +1,12 @@
+{ config, lib, pkgs, dotfilesPath, ... }:
 {
   programs.hyprlock = {
-
     enable = true;
+  };
 
-    settings = {
-
-      general = {
-        hide_cursor = true;
-        no_fade_in = false;
-      };
-
-      animations = {
-        enabled = true;
-        bezier = "linear, 1, 1, 0, 0";
-        animation =  [
-          "fadeIn, 1, 5, linear"
-          "fadeOut, 1, 5, linear"
-          "inputFieldDots, 1, 2, linear"
-        ];
-      };
-
-      background = {
-        monitor = [ "" ];
-        # path = "screenshot";
-        blur_passes = 3;
-      };
-
-      input-field = {
-        monitor = [ "" ];
-        size = "20%, 5%";
-        outline_thickness = 3;
-        inner_color = "rgba(0, 0, 0, 0.0)"; # no fill
-        outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
-        fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
-        font_color = "rgb(143, 143, 143)";
-        fade_on_empty = false;
-        rounding = 15;
-        font_family = "JetBrainsMono Nerd Font";
-        placeholder_text = "Input Password...";
-        dots_spacing = 0.3;
-        position = "0, -20";
-        halign = "center";
-        valign = "center";
-      };
-
-      label = {
-        monitor = [ "" ];
-        text = ''
-          cmd[update:60000] date +"%A, %d %B %Y"
-        ''; # update every 60 seconds
-        font_size = 25;
-        font_family = "JetBrainsMono Nerd Font";
-        position = "-30, -150";
-        halign = "right";
-        valign = "top";
-      };
-
-    };
+  # Symlink the config file from the dotfiles repo
+  xdg.configFile."hypr/hyprlock.conf" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/Hyprlock/hyprlock.conf";
   };
 
 }
